c2048617c57dc434ec21deeb68f372142fa088c6
krasnov <maxmost512@gmail.com>
2020-01-06 11:45:11 +0200

    modal window refactor
    refactor of js function file for modal window
    refactor of modal window html markup

---
+++ src/assets/js/modalWindowNew.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const wrapperElem = document.getElementById('modalWrapper');
+const imageElem = document.getElementById('imageSrc');
+const source = document.getElementById('works');
+
+wrapperElem.onclick = () => {
+  styleSwitch();
+};
+
+source.onclick = function(event) {
+  let target = event.target;
+  setSrcAttr(target);
+};
+
+function styleSwitch() {
+  wrapperElem.classList.toggle('overlayViewWrapperOpen');
+}
+
+function setSrcAttr(target) {
+  imageElem.setAttribute('src', `${getSrcAttr(target)}`);
+  styleSwitch();
+}
+
+function getSrcAttr(target) {
+  //сделать обход childNodes на поиск элемента img
+  // for..of ??
+  return target.childNodes[3].getAttribute('src');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import './assets/js/navigation.js';
 import './assets/js/asyncDataLoad.js';
 import './assets/js/slider.js';
-import './assets/js/modalWindow.js';
+import './assets/js/modalWindowNew.js';
+
 
 import './assets/css/reset.css';
 import './assets/scss/styles.scss';
