d52156be0cfdbabd20925d71f9925f4f805b560f
krasnov <maxmost512@gmail.com>
2020-01-08 13:02:20 +0200

    slider refactor

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,81 +1,74 @@
 'use strict';
 const sliderImage = document.getElementById('sliderImage');
-const dots = document.getElementsByClassName('dot');
+// const dots = document.getElementsByClassName('dot');
+const dots = document.querySelectorAll('#slider>div>div>div');
 const sliderInfoMain = document.getElementById('sliderInfoMain');
 const sliderInfoQuote = document.getElementById('sliderInfoQuote');
 
+const infoQuote = [
+  'SUSAN SIMS, INTERACTION DESIGNER AT XYZ',
+  'SUSAN SIMS, INTERACTION DESIGNER AT XYZ'];
 
-dots[0].onclick = () => {
-  sliderImage.src = './assets/images/testimonial-1.jpg';  //сделать по смене класса и прикрутить анимацию
-  dotStyleChanger();
-  sliderInfoMain.innerText = '"Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Aenean eu leo quam..."';
-  sliderInfoQuote.innerText = 'SUSAN SIMS, INTERACTION DESIGNER AT XYZ';
-};
-dots[1].onclick = () => {
-  sliderImage.src = './assets/images/testimonial-2.jpg';
-  dotStyleChanger();
-  sliderInfoMain.innerText = '"Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur..."';
-  sliderInfoQuote.innerText = 'SUSAN SIMS, INTERACTION DESIGNER AT XYZ';
+const infoMain = [
+  '"Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Aenean eu leo quam..."',
+  '"Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui. Aenean lacinia bibendum nulla sed consectetur..."'];
 
-};
+const imageSrc = [
+  './assets/images/testimonial-1.jpg', './assets/images/testimonial-2.jpg'];
 
-function dotStyleChanger() {
-  dots[1].classList.toggle('activeDot');
-  dots[1].classList.toggle('fade');
-  dots[0].classList.toggle('activeDot');
-  dots[0].classList.toggle('fade');
+let currentIndex = 0;
+let previousIndex;
+
+//adds eventListeners to the Child Nodes
+for (let dot of dots) {
+  dot.addEventListener('click', onClickDot);
 }
 
-/*
-
- const imageSrc = [
- './assets/images/testimonial-1.jpg',
- './assets/images/testimonial-2.jpg'];
-
- class slider111 {
- constructor(imageSrc, currentSlide) {
- this.imageSrc = imageSrc;
- this.currentSlide = currentSlide;
- this.currentSlideIndex = 0;
- this.maxLength = 1;
- }
-
- get currentIndex() {
- return this.currentSlideIndex;
- }
-
- set currentIndex(value) {
- if (isNaN(value)) {
- throw new TypeError();
- }
- if (value < 0 || value > this.maxLength) {
- throw new RangeError();
- }
- this.currentSlideIndex = value;
- }
-
- getNextIndex(index, length) {
- if (index < 0 || index > length - 1) {
- throw new RangeError();
- }
- return (index + 1) % length;
- }
-
- goNext() {
- this.currentIndex = this.getNextIndex(this.currentIndex(), this.maxLength);
- }//currentSlideIndex
-
- goNextSlide = () => {
-
- const currentIndex = this.currentIndex;
- const nextIndex = this.getNextIndex(this.currentIndex(), this.maxLength);
+setTimeout(() => {
+  onClickDot();
+  setTimeout(onClickDot, 5000);
+}, 5000);
 
 
- currentSlide.classList.toggle('activeSlide');
- nextSlide.classList.toggle('activeSlide');
+function onClickDot() {
 
- this.goNext();
- };
- }
- */
+  currentIndex = (currentIndex + 1) % imageSrc.length;
+  previousIndex = (currentIndex - 1 + imageSrc.length) % imageSrc.length;
+  dots[currentIndex].classList.add('activeDot');
+  dots[previousIndex].classList.remove('activeDot');
+  updateImage();
+  updateInfo(currentIndex);
+
+}
+
+sliderImage.onload = () => {
+  sliderImage.style.opacity = '1';
+  sliderInfoMain.style.opacity = '1';
+  sliderInfoQuote.style.opacity = '1';
+
+};
+
+function updateImage() {
+  sliderImage.style.opacity = '0';
+  setTimeout(() => {
+    sliderImage.src = imageSrc[currentIndex];
+  }, 250);
+  sliderImage.onload = () => {
+    sliderImage.style.opacity = '1';
+  };
+
+}
+
+function updateInfo(currentIndex) {
+  sliderInfoMain.style.opacity = '0';
+  sliderInfoQuote.style.opacity = '0';
+  setTimeout(() => {
+    sliderInfoMain.innerText = infoMain[currentIndex];
+    sliderInfoQuote.innerText = infoQuote[currentIndex];
+  }, 250);
+  sliderInfoMain.style.opacity = '1';
+  sliderInfoQuote.style.opacity = '1';
+}
+
+
 
