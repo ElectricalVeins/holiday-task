1382183d89c82b2955013272f8cbe14792ad045c
krasnov <maxmost512@gmail.com>
2020-01-12 00:21:18 +0200

    css bugfix

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,73 +1,34 @@
-
 'use strict';
 
+import {toggleClass} from '../../utils';
 
 const headerElem = document.getElementById('headerWrapper');
-const ulElem = document.getElementById('navigationList');
-const liElem = document.querySelectorAll('body>header>nav>ul>li');
+const burgerElem = document.getElementById('menuBtn');
+const liElems = document.querySelectorAll('body>header>div>nav>ul>li');
 
-const downloadBttnElem = document.getElementById('downloadBttn');
-const burgerButton = document.getElementById('menuBtn');
+document.onscroll = scrollNavStyle;
+document.onload = scrollNavStyle;
 
-burgerButton.onclick = () => {
-  burgerButtonStyleChange();
-  buttonFunction();
+burgerElem.onclick = () => {
+  onClickClassChange();
+
+  liElems.forEach((li) => {
+    li.onclick = () => {
+      onClickClassChange();
+    };
+  });
 };
 
-//For the header onscroll style change
-document.onscroll = eventListeners;
-document.onload = eventListeners;
-
-function eventListeners() {
-  scrollNavStyle();
-  downloadButtonAnimation();
-}
-
 function scrollNavStyle() {
-  if (window.scrollY > 51) {
+  if (window.scrollY > 40) {
     headerElem.classList.add('pageOnScroll');
   } else {
     headerElem.classList.remove('pageOnScroll');
   }
 }
 
-//Button Animation
-function burgerButtonStyleChange() {
-  burgerButton.classList.toggle('change');
+
+function onClickClassChange() {
+  toggleClass(burgerElem, 'change');
+  toggleClass(document.getElementById('navigationList'), 'ulWrapper');
 }
-
-function extNavStyleToggler() {
-  ulElem.classList.toggle('navigation');
-  ulElem.classList.toggle('popUpNavigation');
-
-}
-
-function onclickNavigationToggle() {
-  extNavStyleToggler();
-
-  liElem.forEach((li) => {
-    li.classList.toggle('popUpNavItem');
-  });
-}
-
-function buttonFunction() {
-
-  extNavStyleToggler();
-
-  liElem.forEach((li) => {
-    li.classList.toggle('popUpNavItem');
-    li.onclick = () => {
-      burgerButtonStyleChange();
-      onclickNavigationToggle();
-    };
-  });
-}
-
-function downloadButtonAnimation() {
-  if (window.scrollY > downloadBttnElem.offsetTop -
-      document.documentElement.clientHeight / 2) {
-    downloadBttnElem.classList.add('downloadButtonAnim');
-  }
-}
-
-
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import './assets/js';
 import './components/EmployeeList';
 /*import './components/Slider';*/
 import './components/ModalWindow';
-import './components/Navigation/nav.js';
+import './components/Navigation';
