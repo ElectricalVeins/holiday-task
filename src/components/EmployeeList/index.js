4998d79542bac6c564aad8fe82a60db398255278
krasnov <maxmost512@gmail.com>
2020-01-13 13:09:52 +0200

    slider refactor
    divide slider on 2 classes

--- src/components/EmployeeList/index.js
+++ src/components/EmployeeList/index.js
@@ -12,6 +12,7 @@ loadJson(EMPLOYEES_JSON_SRC)
     .then(teamListCreator);
 
 function teamListCreator(array) {
+
   const teamContainer = document.getElementById('teamContainer');
   for (let item of array) {
     teamContainer.appendChild(EmployeeElemCreator(item));
